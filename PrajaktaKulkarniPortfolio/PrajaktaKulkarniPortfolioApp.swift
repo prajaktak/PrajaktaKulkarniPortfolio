05b1381827dbdd6ab9fe58e132e567489f9cb377
Prajakta Kulkarni <prachee.j@gmail.com>
2026-01-28 15:13:43 +0100

    Initial commit: Create Xcode project for Portfolio app

---
+++ PrajaktaKulkarniPortfolio/PrajaktaKulkarniPortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrajaktaKulkarniPortfolioApp.swift
+//  PrajaktaKulkarniPortfolio
+//
+//  Created by Prajakta Kulkarni on 28/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct PrajaktaKulkarniPortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PrajaktaKulkarniPortfolioTests/PrajaktaKulkarniPortfolioTests.swift
@@ -0,0 +1,17 @@
+//
+//  PrajaktaKulkarniPortfolioTests.swift
+//  PrajaktaKulkarniPortfolioTests
+//
+//  Created by Prajakta Kulkarni on 28/01/2026.
+//
+
+import Testing
+@testable import PrajaktaKulkarniPortfolio
+
+struct PrajaktaKulkarniPortfolioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
