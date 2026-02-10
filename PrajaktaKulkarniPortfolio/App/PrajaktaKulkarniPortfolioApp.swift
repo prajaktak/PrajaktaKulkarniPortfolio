ddb1ae3c4420466c0707e1f91ea766d6f5906cef
Prajakta Kulkarni <prachee.j@gmail.com>
2026-02-10 19:52:17 +0100

    Initialize Firebase in app, add GoogleService-Info.plist, and fix bundle ID to match Firebase configuration

--- PrajaktaKulkarniPortfolio/App/PrajaktaKulkarniPortfolioApp.swift
+++ PrajaktaKulkarniPortfolio/App/PrajaktaKulkarniPortfolioApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct PrajaktaKulkarniPortfolioApp: App {
+
+    init() {
+        FirebaseApp.configure()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
